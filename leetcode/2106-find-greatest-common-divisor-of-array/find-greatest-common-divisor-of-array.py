792ab743c016b34ce69aee0ab921aa722e764000
Tamirat kebede <157843654+kika1s3@users.noreply.github.com>
2024-03-27 15:37:01 +0300

    Time: 58 ms (34.50%) | Memory: 16.8 MB (14.61%) - LeetSync

--- leetcode/2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
+++ leetcode/2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -2,9 +2,8 @@ class Solution:
     def findGCD(self, nums: List[int]) -> int:
         minim = min(nums)
         maxim = max(nums)
-        def gcd(a, b):
-            if b == 0:
-                return a
-            return gcd(b, a%b)
-        return gcd(minim, maxim)
-        
+        while maxim !=0:
+            rem = minim % maxim
+            minim = maxim
+            maxim = rem
+        return minim
